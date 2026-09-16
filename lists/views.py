76afbecd716f848f2f307223c1a30dbe97ab3fae
Diego Silva <diegosilvadpf@gmail.com>
2026-09-16 19:40:06 -0300

    Refatora view para usar o template home.html

--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,4 @@
-from django.shortcuts import render, redirect
-from lists.models import Item
+from django.shortcuts import render
 
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST.get('item_text', '')
-        if new_item_text:
-            Item.objects.create(text=new_item_text)
-        return redirect('/')
-
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html')
